7e5ea60419ad6a03b9e3355da72e878630983db7
Nico Kaiser <nico@kaiser.me>
2025-02-06 19:39:00 +0100

    update

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,54 @@
-// import { JustifiedLayout } from '@immich/justified-layout-wasm';
+import { JustifiedLayout as JustifiedLayoutWasm } from '@immich/justified-layout-wasm';
 import { JustifiedLayout } from './justified-layout';
 import justifiedLayout from 'justified-layout';
 
-const rowHeight = 100;
+const rowHeight = 120;
 const spacing = 4;
 const heightTolerance = 0.25;
 
-// ---
+const aspectRatios = [1.5, 0.6666666666666666, 1.3274336283185841, 1.3333333333333333, 0.7516666666666667, 1.5, 0.665, 1.4018691588785046, 1.3392857142857142];
 
 const app = document.getElementById("app");
-
-const aspectRatios = [1.5, 0.6666666666666666, 1.3274336283185841, 1.3333333333333333, 0.7516666666666667, 1.5, 0.665, 1.4018691588785046, 1.3392857142857142];
-let containerWidth = 0;
 
 const container = document.createElement('div');
 app.appendChild(container);
 
-const info = document.createElement('p');
-info.innerHTML = `spacing: ${spacing}<br>rowHeight: ${rowHeight} ± ${heightTolerance} (${rowHeight - (rowHeight * heightTolerance)}..${rowHeight + (rowHeight * heightTolerance)})`;
-app.appendChild(info);
-
-const galleryOld = document.createElement('div');
-galleryOld.style.background = 'lightgrey';
-galleryOld.style.position = 'relative';
-galleryOld.style.marginBottom = '10px';
-
+// justified-layout
+const headerFlickr = document.createElement('h3');
+headerFlickr.innerHTML = 'justified-layout';
+container.appendChild(headerFlickr);
+const galleryFlickr = document.createElement('div');
+galleryFlickr.style.background = 'lightgrey';
+galleryFlickr.style.position = 'relative';
+galleryFlickr.style.marginBottom = '10px';
 for (let i = 0; i < aspectRatios.length; i++) {
     const item = document.createElement('div');
     item.style.background = 'grey';
-    galleryOld.appendChild(item);
+    galleryFlickr.appendChild(item);
 }
-const galleryOldItems = galleryOld.querySelectorAll('div');
-container.appendChild(galleryOld);
+const galleryFlickrItems = galleryFlickr.querySelectorAll('div');
+container.appendChild(galleryFlickr);
 
+// @immich/justified-layout-wasm
+const headerImmich = document.createElement('h3');
+headerImmich.innerHTML = '@immich/justified-layout-wasm';
+container.appendChild(headerImmich);
+const galleryImmich = document.createElement('div');
+galleryImmich.style.background = 'lightgrey';
+galleryImmich.style.position = 'relative';
+galleryImmich.style.marginBottom = '10px';
+for (let i = 0; i < aspectRatios.length; i++) {
+    const item = document.createElement('div');
+    item.style.background = 'grey';
+    galleryImmich.appendChild(item);
+}
+const galleryImmichItems = galleryImmich.querySelectorAll('div');
+container.appendChild(galleryImmich);
+
+// updated JS implementation
+const headerNew = document.createElement('h3');
+headerNew.innerHTML = 'updated JS implementation';
+container.appendChild(headerNew);
 const galleryNew = document.createElement('div');
 galleryNew.style.background = 'lightgrey';
 galleryNew.style.position = 'relative';
@@ -44,45 +60,63 @@ for (let i = 0; i < aspectRatios.length; i++) {
 const galleryNewItems = galleryNew.querySelectorAll('div');
 container.appendChild(galleryNew);
 
+const info = document.createElement('p');
+info.innerHTML = `spacing: ${spacing}<br>rowHeight: ${rowHeight} ± ${heightTolerance} (${rowHeight - (rowHeight * heightTolerance)}..${rowHeight + (rowHeight * heightTolerance)})`;
+container.appendChild(info);
+
+let containerWidth = 0;
+
 function updateGallery() {
     if (!app) return;
     if (containerWidth === container.getBoundingClientRect().width) return;
     containerWidth = container.getBoundingClientRect().width;
 
     // justified-layout
-    const layoutOld = justifiedLayout(aspectRatios, {
+    const layoutFlickr = justifiedLayout(aspectRatios, {
         targetRowHeight: rowHeight,
         containerWidth,
         boxSpacing: spacing,
         targetRowHeightTolerance: heightTolerance,
         containerPadding: 0,
     });
-
-    // galleryOld.style.width = containerWidth + 'px';
-    galleryOld.style.height = layoutOld.containerHeight + "px";
-
-    for (let i = 0; i < galleryOldItems.length; i++) {
-        const item = galleryOldItems[i];
+    galleryFlickr.style.height = layoutFlickr.containerHeight + "px";
+    for (let i = 0; i < aspectRatios.length; i++) {
+        const item = galleryFlickrItems[i];
         item.style.position = "absolute";
-        item.style.top = layoutOld.boxes[i].top + "px";
-        item.style.left = layoutOld.boxes[i].left + "px";
-        item.style.width = layoutOld.boxes[i].width + "px";
-        item.style.height = layoutOld.boxes[i].height + "px";
-        item.innerHTML = `${Math.round(layoutOld.boxes[i].width)}x${Math.round(layoutOld.boxes[i].height)}`
+        item.style.top = layoutFlickr.boxes[i].top + "px";
+        item.style.left = layoutFlickr.boxes[i].left + "px";
+        item.style.width = layoutFlickr.boxes[i].width + "px";
+        item.style.height = layoutFlickr.boxes[i].height + "px";
+        item.innerHTML = `${Math.round(layoutFlickr.boxes[i].width)}x${Math.round(layoutFlickr.boxes[i].height)}`
     }
 
     // @immich/justified-layout-wasm
+    const layoutImmich = new JustifiedLayoutWasm(new Float32Array(aspectRatios), {
+        rowHeight,
+        rowWidth: containerWidth,
+        spacing,
+        heightTolerance,
+    });
+    galleryImmich.style.height = layoutImmich.containerHeight + "px";
+    for (let i = 0; i < aspectRatios.length; i++) {
+        const item = galleryImmichItems[i];
+        item.style.position = "absolute";
+        item.style.top = layoutImmich.getTop(i) + "px";
+        item.style.left = layoutImmich.getLeft(i) + "px";
+        item.style.width = layoutImmich.getWidth(i) + "px";
+        item.style.height = layoutImmich.getHeight(i) + "px";
+        item.innerHTML = `${layoutImmich.getWidth(i)}x${layoutImmich.getHeight(i)}`;
+    }
+
+    // updated JS implementation
     const layoutNew = new JustifiedLayout(aspectRatios, {
         rowHeight,
         rowWidth: containerWidth,
         spacing,
         heightTolerance,
     });
-
-    // galleryNew.style.width = Math.max(containerWidth, layoutNew.containerWidth) + 'px';
     galleryNew.style.height = layoutNew.containerHeight + "px";
-
-    for (let i = 0; i < galleryNewItems.length; i++) {
+    for (let i = 0; i < aspectRatios.length; i++) {
         const item = galleryNewItems[i];
         item.style.position = "absolute";
         item.style.top = layoutNew.boxes[i].top + "px";
